28351ed79a19b10ee6d6cb5bcc47a3af0a576b41
Steve White <swhite22@student.waubonsee.edu>
2019-03-28 12:00:22 -0500

    fixed grammar on output message

--- two_number_checker.py
+++ two_number_checker.py
@@ -2,4 +2,4 @@ num1 = input('please input your first number')
 num2 = input('please input your second number')
 
 if num1>num2:
-    print("num 1 is bigger than num 2")
+    print("Number 1 is bigger than Number 2.")
