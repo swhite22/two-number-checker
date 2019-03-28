4f722b147d18e37840680418df8d2449fba942fa
Steve White <swhite22@student.waubonsee.edu>
2019-03-28 12:06:21 -0500

    added check for equality

--- two_number_checker.py
+++ two_number_checker.py
@@ -2,6 +2,8 @@ num1 = input('please input your first number')
 num2 = input('please input your second number')
 
 if num1>num2:
-    print("num 1 is bigger than num 2")
+    print("Number 1 is bigger than Number 2.")
+elif num2>num1:
+    print("Number 2 is bigger than Number 1.")
 else:
-    print("num2 is bigger than num1")
+    print("Number 1 and Number 2 are equal.")
